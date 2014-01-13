f982819f857c1b1e84098760c254800c5f8333e3
kevinx6000 <kevinx6000@gmail.com>
2014-01-14 01:01:14 +0800

    完成偏好搜尋

--- Project2/src/Liveinfo.java
+++ Project2/src/Liveinfo.java
@@ -1,19 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-
+// 住宿資訊
 public class Liveinfo {
-
-	// 連線
-	private Connection con;
 	
-	// 印出前10個城市
+	// 印出前10個地址
 	public void printtop10fromCity(String City, Database db)
 	{
 		try{
 			// 取得該資料庫的連線
-			con = db.con;
+			Connection con = db.con;
 			
 			// 敘述子
 			Statement st = con.createStatement();
@@ -43,4 +42,66 @@ public class Liveinfo {
 			System.out.println(ee.getMessage());
 		}
 	}
+
+	// 印出符合條件的搜尋結果
+	public void printResult(String City, Database db)
+	{
+		try{
+			// 取得該資料庫的連線
+			Connection con = db.con;
+			
+			// 敘述子
+			Statement st = con.createStatement();
+			
+			// 符合該縣市的項目
+			String sql = "SELECT * FROM live WHERE address LIKE '%"+City+"%'";
+			
+			// 紀錄偏好
+			String prefer="";
+			
+			// Reading buffer
+			BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+			
+			// 不斷讀入偏好
+			System.out.println("住宿搜尋: 請輸入偏好，一行一個，輸入'.'離開");
+			while(true)
+			{
+				// 偏好
+				prefer = buf.readLine();
+				
+				// 停止讀入
+				if(prefer.equals(".")) break;
+				
+				// 增加偏好搜尋
+				sql += " and Description LIKE '%"+prefer+"%'";
+			}
+			sql += ";";
+			
+			// 取得Response
+			ResultSet rs = st.executeQuery(sql);
+			
+			// 列出所有符合項目(最多前10個)
+			int i=0;
+			System.out.println("<住宿資訊>");
+			while (rs.next()&&i<10) {
+				
+				// 印出名稱及地址
+				System.out.println("("+(i+1)+")--------------");
+				System.out.println("名稱: "+rs.getString("title"));
+				System.out.println("地址: "+rs.getString("address"));
+				
+				// 遞增
+				i++;
+			}
+			System.out.println("");
+			st.close();
+			
+		}catch(Exception ee){
+			
+			// 錯誤訊息
+			System.out.println("取得住宿資訊失敗");
+			System.out.println(ee.getMessage());
+		}
+	}
+	
 }
--- Project2/src/Project2.java
+++ Project2/src/Project2.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 // 主類別
 public class Project2 {
@@ -13,19 +16,29 @@ public class Project2 {
 		db.start_link();
 		
 		// 目的地縣市
-		String city = "高雄";
+		String city = "";
+		System.out.print("請輸入城市:");
+		try{
+			// 讀取使用者輸入的縣市
+			BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+			city = buf.readLine();
+		}catch(Exception ee){
+			// 提示訊息
+			System.out.println("取得縣市失敗");
+			System.out.println(ee.getMessage());
+		}
 		
 		// 美食資訊
 		Foodinfo fi = sf.getFoodinfo();
-		fi.printtop10fromCity(city, db);
+		fi.printResult(city, db);
 		
 		// 景點資訊
 		Siteinfo si = sf.getSiteinfo();
-		si.printtop10fromCity(city, db);
+		si.printResult(city, db);
 		
 		// 住宿資訊
 		Liveinfo li = sf.getLiveinfo();
-		li.printtop10fromCity(city, db);
+		li.printResult(city, db);
 		
 		// 資料庫結束
 		db.end_link();
